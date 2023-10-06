48e58ab0e465292d4495fb7c1ad35f6c0827e21c
thanhtoantnt <thanhtoantnt@gmail.com>
2023-10-06 10:24:10 +0800

    fix the error for auto-fix

--- src/evm/oracles/v2_pair.rs
+++ src/evm/oracles/v2_pair.rs
@@ -1,8 +1,10 @@
 use crate::evm::input::{ConciseEVMInput, EVMInput};
 use crate::evm::oracle::{dummy_precondition, EVMBugResult};
+use crate::evm::oracles::V2_PAIR_BUG_IDX;
 use crate::evm::producers::pair::PairProducer;
 use crate::evm::types::{bytes_to_u64, EVMAddress, EVMFuzzState, EVMOracleCtx, EVMU256};
 use crate::evm::vm::EVMState;
+use crate::fuzzer::ORACLE_OUTPUT;
 use crate::oracle::{Oracle, OracleCtx, Producer};
 use crate::state::HasExecutionResult;
 use bytes::Bytes;
@@ -14,8 +16,6 @@ use std::collections::HashMap;
 use std::hash::{Hash, Hasher};
 use std::ops::Deref;
 use std::rc::Rc;
-use crate::evm::oracles::V2_PAIR_BUG_IDX;
-use crate::fuzzer::ORACLE_OUTPUT;
 
 pub struct PairBalanceOracle {
     pub pair_producer: Rc<RefCell<PairProducer>>,
@@ -27,8 +27,19 @@ impl PairBalanceOracle {
     }
 }
 
-impl Oracle<EVMState, EVMAddress, Bytecode, Bytes, EVMAddress, EVMU256, Vec<u8>, EVMInput, EVMFuzzState, ConciseEVMInput>
-    for PairBalanceOracle
+impl
+    Oracle<
+        EVMState,
+        EVMAddress,
+        Bytecode,
+        Bytes,
+        EVMAddress,
+        EVMU256,
+        Vec<u8>,
+        EVMInput,
+        EVMFuzzState,
+        ConciseEVMInput,
+    > for PairBalanceOracle
 {
     fn transition(&self, _ctx: &mut EVMOracleCtx<'_>, _stage: u64) -> u64 {
         0
@@ -46,54 +57,10 @@ impl Oracle<EVMState, EVMAddress, Bytecode, Bytes, EVMAddress, EVMU256, Vec<u8>,
             Vec<u8>,
             EVMInput,
             EVMFuzzState,
-            ConciseEVMInput
+            ConciseEVMInput,
         >,
         stage: u64,
     ) -> Vec<u64> {
-        let mut violations = vec![];
-        #[cfg(feature = "flashloan_v2")]
-        {
-            for (addr, (r0, r1)) in &self.pair_producer.deref().borrow().reserves {
-                match ctx
-                    .fuzz_state
-                    .get_execution_result()
-                    .new_state
-                    .state
-                    .flashloan_data
-                    .prev_reserves.get(addr) {
-                    Some((pre_r0, pre_r1)) => {
-                        if *pre_r0 == *r0 && *pre_r1 > *r1 || *pre_r1 == *r1 && *pre_r0 > *r0 {
-                            // calculate hash in u64 of pair address (addr) using DefaultHasher
-                            let mut hasher = DefaultHasher::new();
-                            addr.hash(&mut hasher);
-                            let hash = hasher.finish();
-                            let bug_idx = hash << 8 + V2_PAIR_BUG_IDX;
-
-                            EVMBugResult::new_simple(
-                                "imbalanced_pair".to_string(),
-                                bug_idx,
-                                format!(
-                                    "{:?}, Reserves changed from {:?} to {:?}\n",
-                                    addr,
-                                    (r0, r1),
-                                    (pre_r0, pre_r1)
-                                ),
-                                ConciseEVMInput::from_input(ctx.input, ctx.fuzz_state.get_execution_result()),
-                            ).push_to_output();
-
-                            violations.push(bug_idx);
-                        }
-                    }
-                    None => {
-                        continue;
-                    }
-                }
-            }
-        }
-        #[cfg(not(feature = "flashloan_v2"))]
-        {
-            panic!("Flashloan v2 required to use pair (-p).")
-        }
-        violations
+        panic!("Flashloan v2 required to use pair (-p).")
     }
 }
